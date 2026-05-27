7f5092717df951008082f5caa7b1d3dece70cfaa
priya8957-dell <maddeshiyapriyanshu2@gmail.com>
2026-05-27 20:04:12 +0530

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "/api";
 
 export const api = async (endpoint, options = {}) => {
   const token = localStorage.getItem("db_token");
